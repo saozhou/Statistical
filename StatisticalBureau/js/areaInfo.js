ad098f3f5126511f203a9ead354743485dbf1f45
770363647 <z770363647@126.com>
2017-10-27 21:51:30 +0800

    时间：10/27/21：51
    完善管理员界面

--- StatisticalBureau/js/areaInfo.js
+++ StatisticalBureau/js/areaInfo.js
@@ -73,35 +73,27 @@ function select_bt_click(pos) {
 	switch(pos) {
 		//国家下拉选择框点击事件
 		case 0:
-			var visiblity = countryListIsShow == false ? "visible" : "hidden";
-			var opacity = countryListIsShow == false ? 1 : 0;
+			var visiblity = countryListIsShow == false ? "block" : "none";
 			countryListIsShow = countryListIsShow == false ? true : false;
-			$(".country ul").eq(0).css("visibility", visiblity);
-			$(".country ul").eq(0).css("opacity", opacity);
+			$(".country ul").eq(0).css("display", visiblity);
 			break;
 			//省级行政区下拉选择框点击事件
 		case 1:
-			var visiblity = provinceListIsShow == false ? "visible" : "hidden";
-			var opacity = provinceListIsShow == false ? 1 : 0;
+			var visiblity = provinceListIsShow == false ? "block" : "none";
 			provinceListIsShow = provinceListIsShow == false ? true : false;
-			$(".province ul").eq(0).css("visibility", visiblity);
-			$(".province ul").eq(0).css("opacity", opacity);
+			$(".province ul").eq(0).css("display", visiblity);
 			break;
 			//市级行政区下拉选择框点击事件
 		case 2:
-			var visiblity = cityListIsShow == false ? "visible" : "hidden";
-			var opacity = cityListIsShow == false ? 1 : 0;
+			var visiblity = cityListIsShow == false ? "block" : "none";
 			cityListIsShow = cityListIsShow == false ? true : false;
-			$(".city ul").eq(0).css("visibility", visiblity);
-			$(".city ul").eq(0).css("opacity", opacity);
+			$(".city ul").eq(0).css("display", visiblity);
 			break;
 			//县级行政区下拉选择框点击事件
 		case 3:
-			var visiblity = countyListIsShow == false ? "visible" : "hidden";
-			var opacity = countyListIsShow == false ? 1 : 0;
+			var visiblity = countyListIsShow == false ? "block" : "none";
 			countyListIsShow = countyListIsShow == false ? true : false;
-			$(".county ul").eq(0).css("visibility", visiblity);
-			$(".county ul").eq(0).css("opacity", opacity);
+			$(".county ul").eq(0).css("display", visiblity);
 			break;
 	}
 
@@ -110,52 +102,56 @@ function select_bt_click(pos) {
 //国家选择列表选项被点击修改显示值
 function country_list_item_click(pos) {
 	var content = $(".country ul li").eq(pos).text();
+	$(".cur-area span").eq(0).text(content);
 	if(content.length > 4)
 		content = content.substring(0, 5);
 	$(".country span").eq(0).text(content);
-	$(".country ul").eq(0).css("visibility", "hidden");
-	$(".country ul").eq(0).css("opacity", "0");
+	$(".country ul").eq(0).css("display", "none");
 	countryListIsShow = false;
 	//复原省市县
 	$(".province span").eq(0).text("省份");
 	$(".city span").eq(0).text("城市");
 	$(".county span").eq(0).text("县区");
 	getProvince(pos);
+
 }
 //省级行政区选择列表选项被点击修改显示值
 function province_list_item_click(pos) {
 	var content = $(".province ul li").eq(pos).text();
+	$(".cur-area span").eq(1).text(content);
 	if(content.length > 4)
 		content = content.substring(0, 5);
 	$(".province span").eq(0).text(content);
-	$(".province ul").eq(0).css("visibility", "hidden");
-	$(".province ul").eq(0).css("opacity", "0");
+	$(".province ul").eq(0).css("display", "none");
 	provinceListIsShow = false;
 	$(".city span").eq(0).text("城市");
 	$(".county span").eq(0).text("县区");
 	getCity(pos);
+
 }
 //市级行政区选择列表选项被点击修改显示值
 function city_list_item_click(pos) {
 	var content = $(".city ul li").eq(pos).text();
+	$(".cur-area span").eq(2).text(content);
 	if(content.length > 4)
 		content = content.substring(0, 5);
 	$(".city span").eq(0).text(content);
-	$(".city ul").eq(0).css("visibility", "hidden");
-	$(".city ul").eq(0).css("opacity", "0");
+	$(".city ul").eq(0).css("display", "none");
 	cityListIsShow = false;
 	$(".county span").eq(0).text("县区");
 	getCounty(pos);
+
 }
 //县级行政区选择列表选项被点击修改显示值
 function county_list_item_click(pos) {
 	var content = $(".county ul li").eq(pos).text();
+	$(".cur-area span").eq(3).text(content);
 	if(content.length > 4)
 		content = content.substring(0, 5);
 	$(".county span").eq(0).text(content);
-	$(".county ul").eq(0).css("visibility", "hidden");
-	$(".county ul").eq(0).css("opacity", "0");
+	$(".county ul").eq(0).css("display", "none");
 	countyListIsShow = false;
+
 }
 //将四级联动全部重置为初始状态
 function resetSpinner() {
